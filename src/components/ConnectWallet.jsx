b74e6cdc974bcea421d36bce61faf3188d2d2e53
walterthesmart <nwaugowalter@gmail.com>
2025-12-17 00:40:06 +0100

    feat: implement ConnectWallet UI

--- src/components/ConnectWallet.jsx
+++ src/components/ConnectWallet.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { showConnect, AppConfig, UserSession } from '@stacks/connect';
+
+const appConfig = new AppConfig(['store_write', 'publish_data']);
+const userSession = new UserSession({ appConfig });
 
 const ConnectWallet = () => {
+    const [userData, setUserData] = useState(null);
+
+    const handleConnect = () => {
+        // logic will be implemented in next branch
+        console.log("Connect clicked");
+    };
+
+    if (userData) {
+        return (
+            <div className="connect-wallet">
+                <p>Connected: {userData.profile.stxAddress.mainnet}</p>
+            </div>
+        )
+    }
+
     return (
         <div className="connect-wallet">
-            {/* TODO: button */}
+            <button onClick={handleConnect} className="btn-connect">
+                Connect Wallet
+            </button>
         </div>
     );
 };
