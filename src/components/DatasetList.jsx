51e8b315e3fe629a658989452718fb82b4a95325
walterthesmart <nwaugowalter@gmail.com>
2025-12-17 00:44:11 +0100

    feat: setup DatasetList component

---
+++ src/components/DatasetList.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+const DatasetList = () => {
+    return (
+        <div className="dataset-list">
+            <h2>Available Datasets</h2>
+            <div className="list-container">
+                {/* List items */}
+            </div>
+        </div>
+    );
+};
+
+export default DatasetList;
