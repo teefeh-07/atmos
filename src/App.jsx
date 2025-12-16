709c910216842797cd52a6290e350cd3c62b11cc
walterthesmart <nwaugowalter@gmail.com>
2025-12-17 00:45:25 +0100

    feat: assemble App with components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import ConnectWallet from './components/ConnectWallet';
+import RegisterDataset from './components/RegisterDataset';
+import DatasetList from './components/DatasetList';
 
 function App() {
     return (
         <div className="App">
-            <h1>Atmos Data Registry</h1>
+            <header>
+                <h1>Atmos Data Registry</h1>
+                <ConnectWallet />
+            </header>
+            <main>
+                <RegisterDataset />
+                <DatasetList />
+            </main>
         </div>
     );
 }
