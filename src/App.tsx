d66651e34a46a923632d02d83ad4ce7ad281709c
teefeh_07 <boluwatifeoniyilo@gmail.com>
2025-12-16 13:21:14 +0100

    feat: add App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className="app-container" style={{ padding: '20px', fontFamily: 'sans-serif' }}>
+            <h1>Atmos Data Registry</h1>
+            <p>Welcome to the Atmos decentralized data registry.</p>
+        </div>
+    );
+}
+
+export default App;
