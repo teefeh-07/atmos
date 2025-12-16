0c9e703ecc0006777fcb5d32c2d6494db6ce7a24
walterthesmart <nwaugowalter@gmail.com>
2025-12-17 00:42:40 +0100

    feat: add coordinate fields to RegisterDataset

--- src/components/RegisterDataset.jsx
+++ src/components/RegisterDataset.jsx
@@ -3,6 +3,11 @@ import React, { useState } from 'react';
 const RegisterDataset = () => {
     const [name, setName] = useState('');
     const [description, setDescription] = useState('');
+    const [coords, setCoords] = useState({ altMin: 0, altMax: 0, lat: 0, long: 0 });
+
+    const handleCoordChange = (field, value) => {
+        setCoords(prev => ({ ...prev, [field]: value }));
+    };
 
     return (
         <div className="register-dataset">
@@ -24,6 +29,22 @@ const RegisterDataset = () => {
                         onChange={(e) => setDescription(e.target.value)}
                         maxLength={500}
                     />
+                </div>
+                <div className="form-group">
+                    <label>Altitude Min:</label>
+                    <input type="number" value={coords.altMin} onChange={(e) => handleCoordChange('altMin', parseInt(e.target.value))} />
+                </div>
+                <div className="form-group">
+                    <label>Altitude Max:</label>
+                    <input type="number" value={coords.altMax} onChange={(e) => handleCoordChange('altMax', parseInt(e.target.value))} />
+                </div>
+                <div className="form-group">
+                    <label>Latitude:</label>
+                    <input type="number" value={coords.lat} onChange={(e) => handleCoordChange('lat', parseInt(e.target.value))} />
+                </div>
+                <div className="form-group">
+                    <label>Longitude:</label>
+                    <input type="number" value={coords.long} onChange={(e) => handleCoordChange('long', parseInt(e.target.value))} />
                 </div>
                 {/* Form fields */}
             </form>
