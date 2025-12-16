3fd3d4dd0f73705b95854de802ae0e418b4f7930
walterthesmart <nwaugowalter@gmail.com>
2025-12-17 00:40:49 +0100

    feat: setup RegisterDataset component

---
+++ src/components/RegisterDataset.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+const RegisterDataset = () => {
+    return (
+        <div className="register-dataset">
+            <h2>Register Dataset</h2>
+            <form>
+                {/* Form fields */}
+            </form>
+        </div>
+    );
+};
+
+export default RegisterDataset;
