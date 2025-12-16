df6a76c5191780bc111f7e71ec007d1624d49b7e
walterthesmart <nwaugowalter@gmail.com>
2025-12-17 00:39:35 +0100

    feat: setup ConnectWallet component

---
+++ src/components/ConnectWallet.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ConnectWallet = () => {
+    return (
+        <div className="connect-wallet">
+            {/* TODO: button */}
+        </div>
+    );
+};
+
+export default ConnectWallet;
